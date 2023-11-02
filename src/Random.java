03030d0772f32af09d7f1e9033583f388e9a2c57
zulpikarCydeo <102216429+zulpikarCydeo@users.noreply.github.com>
2023-11-02 19:25:50 +0300

    changed the main code

--- src/Random.java
+++ src/Random.java
@@ -1,9 +1,12 @@
 public class Random {
 
     public static void main(String[] args) {
+
         System.out.println("this class is about git practice");
 
         System.out.println("this is new branch");
+
+        System.out.println("this is another change in this branch");
 
     }
 }
